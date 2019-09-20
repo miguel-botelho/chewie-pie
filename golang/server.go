2ff15e9048c6c19713d1d66cbe1500d103b4c289
Miguel Botelho <jmbotelho95@gmail.com>
2019-09-20 15:00:15 +0100

    Refactor message from server

--- golang/server.go
+++ golang/server.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println(formatRequest(r))
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			log.Fatal(err)
@@ -18,31 +15,8 @@ func main() {
 		log.Println(string(body))
 
 		w.WriteHeader(200)
-		w.Write([]byte(`These are the droids you're looking for`))
+		w.Write([]byte(`These are not the droids you're looking for`))
 	})
 
 	http.ListenAndServe(":5000", nil)
-}
-
-// formatRequest generates ascii representation of a request
-func formatRequest(r *http.Request) string {
-	// Create return string
-	var request []string // Add the request string
-	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-	request = append(request, url)                             // Add the host
-	request = append(request, fmt.Sprintf("Host: %v", r.Host)) // Loop through headers
-	for name, headers := range r.Header {
-		name = strings.ToLower(name)
-		for _, h := range headers {
-			request = append(request, fmt.Sprintf("%v: %v", name, h))
-		}
-	}
-
-	// If this is a POST, add post data
-	if r.Method == "POST" {
-		r.ParseForm()
-		request = append(request, "\n")
-		request = append(request, r.Form.Encode())
-	} // Return the request as a string
-	return strings.Join(request, "\n")
 }
